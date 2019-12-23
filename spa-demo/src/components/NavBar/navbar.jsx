70f725b93bc23b8e0d60490b2be3614237b6c2dd
habernet <phaberern@gmail.com>
2019-12-23 11:21:48 -0500

    Added Materialize, and navbar, react-router-dom

---
+++ spa-demo/src/components/NavBar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Navbar = props => (
+    <nav>
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a>PAGE ONE</a></li>
+        <li><a>PAGE TWO</a></li>
+        <li><a>PAGE THREE</a></li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Navbar;
